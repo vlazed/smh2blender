6dedde9dc9365749652ef0a5247433a3b8c81f50
vlazed <dinoboy.valdez4@gmail.com>
2025-03-22 21:32:39 -0500

    Importing fixes and adjustments - Fix EntityData being read as a list by converting to a dictionary - Fix importing data for frames with no modifier data - Change importing logic to only import frame and data pairs. This allows physbones and bones to be imported correctly if they are keyed on different frames

--- smh/bone.py
+++ smh/bone.py
@@ -9,15 +9,17 @@ class GenericBoneField:
     pos: Vector
     ang: Euler
     armature: ArmatureObject
+    frame: float
 
     @staticmethod
     def get_matrix(pos: Vector, ang: Euler, scale: Vector = Vector((1, 1, 1, 1))):
         # Convert to pose space
         return Matrix.Translation(pos) @ (ang.to_matrix().to_4x4()) @ Matrix.Diagonal(scale)
 
-    def __init__(self, armature: ArmatureObject, data: GenericBoneDict, angle_offset: Euler = Euler()):
-        self.pos = self.transform_vec(data["Pos"], sign=(1, 1, -1))
+    def __init__(self, armature: ArmatureObject, data: GenericBoneDict, frame: float, angle_offset: Euler = Euler()):
+        self.pos = self.transform_vec(data["Pos"], sign=(1, 1, 1))
         self.ang = self.transform_ang(data["Ang"], angle_offset=angle_offset)
+        self.frame = frame
         self.armature = armature
         self.matrix = self.get_matrix(self.pos, self.ang)
 
@@ -37,7 +39,7 @@ class GenericBoneField:
             Vector: Blender vector in local space
         """
         vec_list = [float(x) for x in vec[1:-1].split(" ")]
-        return Vector((sign[0] * vec_list[0], sign[1] * vec_list[2], sign[2] * vec_list[1]))
+        return Vector((sign[0] * vec_list[0], sign[1] * vec_list[1], sign[2] * vec_list[2]))
 
     def transform_ang(self, ang: str, angle_offset: Euler = Euler()) -> Euler:
         """Transform an SMH angle into a Blender angle in local space.
@@ -60,8 +62,8 @@ class PhysBoneField(GenericBoneField):
     local_ang: Euler | None
     local_matrix: Matrix | None
 
-    def __init__(self, armature: ArmatureObject, data: PhysBoneDict, angle_offset: Euler):
-        super().__init__(armature=armature, data=data, angle_offset=angle_offset)
+    def __init__(self, armature: ArmatureObject, data: PhysBoneDict, frame: float, angle_offset: Euler):
+        super().__init__(armature=armature, data=data, frame=frame, angle_offset=angle_offset)
         self.local_pos = self.local_ang = self.local_matrix = None
         if data.get("LocalPos"):
             self.local_pos = self.transform_local_vec(
@@ -95,7 +97,7 @@ class PhysBoneField(GenericBoneField):
 
         # Switch back the y and z axes for the local vectors
         new_vec = self.transform_vec(vec, sign)
-        return Vector((new_vec[0], new_vec[2], new_vec[1]))
+        return Vector((new_vec[0], new_vec[1], new_vec[2]))
 
     def set_ref_offset(self, refphysbone, is_root: bool):
         """Use the reference mapped physics bone to correct the local angle
@@ -135,8 +137,8 @@ class BoneField(GenericBoneField):
         ang_list = [radians(float(x)) for x in ang[1:-1].split(" ")]
         return Euler((ang_list[2], ang_list[0], ang_list[1]))
 
-    def __init__(self, armature: ArmatureObject, data: BoneDict):
-        super().__init__(armature=armature, data=data)
+    def __init__(self, armature: ArmatureObject, frame: float, data: BoneDict):
+        super().__init__(armature=armature, frame=frame, data=data)
         self.ang = self.transform_manip_ang(data["Ang"])
         if data["Scale"]:
             self.scale = self.transform_vec(data["Scale"]).to_4d()
--- smh/data.py
+++ smh/data.py
@@ -4,11 +4,178 @@ import json
 import os
 from mathutils import Vector, Euler, Matrix
 from math import floor
+from typing import Generator
 
 from .frame import PhysBoneFrames, BoneFrames, PhysBoneTree
-from .types import SMHEntityDict, SMHFileDict, SMHEntityFrameDict, ArmatureObject
+from .types import SMHEntityDict, SMHFileDict, SMHEntityFrameDict, ArmatureObject, BoneMap
 from .bone import PhysBoneField, BoneField
 from .props import SMHMetaData, SMHProperties
+
+
+class SMHImporter:
+    physics_obj_map: BoneMap
+    bone_map: BoneMap
+    action: bpy.types.Action
+    physics_tree: PhysBoneTree
+    armature: ArmatureObject
+    interpolation: list
+
+    def create_fc(
+        self,
+        index: float,
+        frames: list[int],
+        samples: Generator[float, None, None],
+        data_path: str,
+        group_name: str
+    ):
+        num_frames = len(frames)
+        fc: bpy.types.FCurve = self.action.fcurves.new(
+            data_path=data_path, index=index, action_group=group_name)
+        fc.keyframe_points.add(num_frames)
+        fc.keyframe_points.foreach_set(
+            "co", [x for co in zip(frames, samples) for x in co])
+        fc.keyframe_points.foreach_set(
+            "interpolation", self.interpolation)
+        fc.update()
+
+    def __init__(
+            self,
+            physics_obj_map: BoneMap,
+            bone_map: BoneMap,
+            armature: ArmatureObject,
+            action: bpy.types.Action,
+            entity: SMHEntityDict):
+        self.physics_obj_map = physics_obj_map
+        self.bone_map = bone_map
+        self.action = action
+
+        self.armature = armature
+        self.physics_tree = PhysBoneTree(armature, physics_obj_map)
+
+        frames = [
+            frame["Position"] for frame in entity["Frames"] if len(frame["EntityData"]) > 0
+        ]
+        num_frames = len(frames)
+        self.interpolation = [
+            bpy.types.Keyframe.bl_rna.properties["interpolation"].enum_items["LINEAR"].value] * num_frames
+        action.frame_start = min(frames)
+        action.frame_end = max(frames)
+
+    @staticmethod
+    def get_pose(
+        data: list[list[PhysBoneField | BoneField]], index: int,
+        bone: bpy.types.PoseBone, local_condition: bool = False
+    ):
+        matrices: list[Matrix] = None
+        if local_condition:
+            matrices = [row[index].local_matrix for row in data]
+        else:
+            matrices = [row[index].matrix for row in data]
+
+        frames = [row[index].frame for row in data]
+
+        pos = [
+            (matrix.translation.x for matrix in matrices),
+            (matrix.translation.y for matrix in matrices),
+            (matrix.translation.z for matrix in matrices)
+        ]
+
+        ang = [
+            (matrix.to_quaternion().w for matrix in matrices),
+            (matrix.to_quaternion().x for matrix in matrices),
+            (matrix.to_quaternion().y for matrix in matrices),
+            (matrix.to_quaternion().z for matrix in matrices),
+        ] if bone.rotation_mode == 'QUATERNION' else [
+            (matrix.to_euler().x for matrix in matrices),
+            (matrix.to_euler().y for matrix in matrices),
+            (matrix.to_euler().z for matrix in matrices),
+        ]
+
+        return pos, ang, frames
+
+    def fcurves_from_data(
+        self,
+        pos: list[Generator[float, None, None]],
+        ang: list[Generator[float, None, None]],
+        frames: list[float],
+        name: str,
+        bone: bpy.types.PoseBone,
+        location_condition: bool = True,
+        rotation_condition: bool = True
+    ):
+        if location_condition:
+            data_path = bone.path_from_id('location')
+            [self.create_fc(
+                index=index, samples=samples, data_path=data_path,
+                group_name=name, frames=frames) for index, samples in enumerate(pos)]
+
+        if rotation_condition:
+            data_path = bone.path_from_id(
+                'rotation_quaternion'
+                if bone.rotation_mode == 'QUATERNION' else 'rotation_euler'
+            )
+            [self.create_fc(
+                index=index, samples=samples, data_path=data_path,
+                group_name=name, frames=frames) for index, samples in enumerate(ang) if samples is not None]
+
+    def import_physics(self, physbone_data: list[list[PhysBoneField]], metadata: SMHMetaData):
+        # Stop Motion Helper reports the root physics object location as an offset from the ground (about 38 units)
+        # Without this adjustment, the armature will be offset from the ground
+        offset = self.physics_tree.get_bone_from_index(
+            0).bone.matrix_local.translation
+        offset = Vector((offset[0], offset[1], offset[2]))
+        [
+            [
+                physbone.add_pos(-offset)
+                for physbone_index, physbone in enumerate(physbone_row)
+                if physbone_index == 0
+            ]
+            for physbone_row in physbone_data
+        ]
+
+        for index, phys_name in enumerate(self.physics_obj_map):
+            bone = self.armature.pose.bones.get(phys_name)
+            if not bone:
+                continue
+
+            pos, ang, frames = self.get_pose(
+                data=physbone_data,
+                index=index,
+                bone=bone,
+                local_condition=self.physics_tree.get_parent(phys_name) is not None
+            )
+
+            if self.physics_tree.get_parent(phys_name) is None:
+                bone.bone.use_local_location = False
+
+            self.fcurves_from_data(
+                pos,
+                ang,
+                frames,
+                name=phys_name,
+                bone=bone,
+                location_condition=metadata.import_stretch or self.physics_tree.get_parent(phys_name) is None
+            )
+
+    def import_bones(self, bone_data: list[list[BoneField]]):
+        for index, bone_name in enumerate(self.bone_map):
+            bone = self.armature.pose.bones.get(bone_name)
+            if not bone or self.physics_tree.bone_dict.get(bone_name) is not None:
+                continue
+
+            pos, ang, frames = self.get_pose(
+                data=bone_data,
+                index=index,
+                bone=bone,
+            )
+
+            self.fcurves_from_data(
+                pos,
+                ang,
+                frames,
+                name=bone_name,
+                bone=bone,
+            )
 
 
 class SMHEntityData():
@@ -63,15 +230,16 @@ class SMHEntity():
     metadata: SMHMetaData
 
     @staticmethod
-    def load_physbones(entity, armature: ArmatureObject, metadata: SMHMetaData | None = None):
+    def load_physbones(entity: SMHEntityDict, armature: ArmatureObject, metadata: SMHMetaData | None = None):
         return [
             [
                 PhysBoneField(
                     armature=armature, data=datum,
-                    angle_offset=metadata.angle_offset() if metadata else Euler()
+                    angle_offset=metadata.angle_offset() if metadata else Euler(),
+                    frame=frame["Position"]
                 ) for datum in frame["EntityData"]["physbones"].values()
             ]
-            for frame in entity["Frames"] if frame["EntityData"].get("physbones")
+            for frame in entity["Frames"] if dict(frame["EntityData"]).get("physbones")
         ]
 
     @staticmethod
@@ -80,10 +248,11 @@ class SMHEntity():
             [
                 BoneField(
                     armature=armature,
-                    data=datum
+                    data=datum,
+                    frame=frame["Position"]
                 ) for datum in frame["EntityData"]["bones"].values()
             ]
-            for frame in entity["Frames"] if frame["EntityData"].get("bones")
+            for frame in entity["Frames"] if dict(frame["EntityData"]).get("bones")
         ]
 
     @staticmethod
@@ -112,7 +281,8 @@ class SMHEntity():
 
         action = self.armature.animation_data.action
         if not action:
-            # Action should exist in the prior step. This is just here in case it doesn't (and to silence linting errors)
+            # Action should exist in the prior step. This is just here in case it
+            # doesn't (and to silence linting errors)
             return self.data
 
         physics_obj_map = []
@@ -190,101 +360,10 @@ class SMHEntity():
         armature.animation_data_create()
         armature.animation_data.action = action
 
-        # Stop Motion Helper reports the root physics object location as an offset from the ground (about 38 units)
-        # Without this adjustment, the armature will be offset from the ground
-        offset = physics_tree.get_bone_from_index(
-            0).bone.matrix_local.translation
-        offset = Vector((offset[0], offset[2], offset[1]))
-        [
-            [
-                physbone.add_pos(-offset)
-                for physbone_index, physbone in enumerate(physbone_row)
-                if physbone_index == 0
-            ]
-            for physbone_row in physbone_data
-        ]
+        importer = SMHImporter(physics_obj_map, bone_map, armature, action, entity)
 
-        frames = [
-            frame["Position"] for frame in entity["Frames"]
-        ]
-        num_frames = len(frames)
-        interpolation = [
-            bpy.types.Keyframe.bl_rna.properties["interpolation"].enum_items["LINEAR"].value] * num_frames
-        action.frame_start = min(frames)
-        action.frame_end = max(frames)
-
-        def create_fc(index, samples, data_path, group_name):
-            fc: bpy.types.FCurve = action.fcurves.new(
-                data_path=data_path, index=index, action_group=group_name)
-            fc.keyframe_points.add(num_frames)
-            fc.keyframe_points.foreach_set(
-                "co", [x for co in zip(frames, samples) for x in co])
-            fc.keyframe_points.foreach_set(
-                "interpolation", interpolation)
-            fc.update()
-
-        for index, bone_name in enumerate(bone_map):
-            bone = armature.pose.bones.get(bone_name)
-            if not bone or physics_tree.bone_dict.get(bone_name) != None:
-                continue
-
-            matrices = [row[index].matrix for row in bone_data]
-
-            pos = [
-                (matrix.translation.x for matrix in matrices),
-                (matrix.translation.y for matrix in matrices),
-                (matrix.translation.z for matrix in matrices)
-            ]
-
-            ang = [
-                (matrix.to_euler().x for i, matrix in enumerate(matrices)),
-                (matrix.to_euler().y for i, matrix in enumerate(matrices)),
-                (matrix.to_euler().z for i, matrix in enumerate(matrices))
-            ]
-
-            data_path = bone.path_from_id('location')
-            [create_fc(
-                index=index, samples=samples, data_path=data_path,
-                group_name=bone_name) for index, samples in enumerate(pos)]
-
-            data_path = bone.path_from_id('rotation_euler')
-            [create_fc(
-                index=index, samples=samples, data_path=data_path,
-                group_name=bone_name) for index, samples in enumerate(ang)]
-
-        for index, phys_name in enumerate(physics_obj_map):
-            bone = armature.pose.bones.get(phys_name)
-            if not bone:
-                continue
-
-            matrices: list[Matrix] = None
-            if physics_tree.get_parent(phys_name):
-                matrices = [row[index].local_matrix for row in physbone_data]
-            else:
-                matrices = [row[index].matrix for row in physbone_data]
-
-            pos = [
-                (matrix.translation.x for matrix in matrices),
-                (matrix.translation.y for matrix in matrices),
-                (matrix.translation.z for matrix in matrices)
-            ]
-
-            ang = [
-                (matrix.to_euler().x for i, matrix in enumerate(matrices)),
-                (matrix.to_euler().y for i, matrix in enumerate(matrices)),
-                (matrix.to_euler().z for i, matrix in enumerate(matrices))
-            ]
-
-            if metadata.import_stretch or physics_tree.get_parent(phys_name) is None:
-                data_path = bone.path_from_id('location')
-                [create_fc(
-                    index=index, samples=samples, data_path=data_path,
-                    group_name=phys_name) for index, samples in enumerate(pos)]
-
-            data_path = bone.path_from_id('rotation_euler')
-            [create_fc(
-                index=index, samples=samples, data_path=data_path,
-                group_name=phys_name) for index, samples in enumerate(ang)]
+        importer.import_bones(bone_data)
+        importer.import_physics(physbone_data, metadata)
 
         return True, f"Successfully loaded {filename}"
 
